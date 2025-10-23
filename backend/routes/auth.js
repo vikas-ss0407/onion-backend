1b1514bfa40322abdcb7d359d8370ab28dec30b7
VIKAS S S <vikas.ss2023ece@sece.ac.in>
2025-10-23 11:56:57 +0530

    updated auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,13 +3,13 @@ const { signup, login, updateProfile, getProfile, getUserFromToken, logout } = r
 
 const router = express.Router();
 
+// Public routes
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/logout', logout);
 
 // Protected routes
-router.use(getUserFromToken);
-router.put('/update', updateProfile);
-router.get('/profile', getProfile);
+router.put('/update', getUserFromToken, updateProfile);
+router.get('/profile', getUserFromToken, getProfile);
 
 module.exports = router;
